bd7ce4433eeb992df746fd32073b11c5db9445c0
Matthew Persing <persinme@rose-hulman.edu>
2015-09-18 16:44:32 -0400

    Added tick marks and moving hands

--- Program_3/src/RenderEngine.h
+++ Program_3/src/RenderEngine.h
@@ -55,18 +55,34 @@ public:
 		
 		glDrawArrays(GL_LINE_LOOP, model.getObjectStart(0), model.getObjectSize(0));
 
-		glm::mat4 rot = glm::rotate(glm::mat4(1.0f), time, glm::vec3(0, 1, 1));
-		glUniformMatrix4fv(transformSlot, 1, GL_FALSE, &rot[0][0]);
-		glDrawArrays(GL_LINES, model.getObjectStart(1), model.getObjectSize(1));
+		//glm::mat4 rot = glm::rotate(glm::mat4(1.0f), time, glm::vec3(0, 1, 1));
+		//glUniformMatrix4fv(transformSlot, 1, GL_FALSE, &rot[0][0]);
+		
+		glm::mat4 trans = glm::translate(glm::mat4(1.0f), glm::vec3(0.5, 0, 0));
+		for(float i = 0 ; i < 12 ; ++i)
+		{
+			glm::mat4 rot = glm::rotate(glm::mat4(1.0f), (float)(i * M_PI / 6.0f), glm::vec3(0,0,1));
+			glm::mat4 fin = rot * trans;
+			glUniformMatrix4fv(transformSlot, 1, GL_FALSE, &fin[0][0]);
+			glDrawArrays(GL_LINES, model.getObjectStart(1), model.getObjectSize(1));
+		}
 
-		glm::mat4 ident = glm::mat4(1.0f);
+		glm::mat4 rot = glm::rotate(glm::mat4(1.0f), time, glm::vec3(0, 0, 1));
+		glUniformMatrix4fv(transformSlot, 1, GL_FALSE, &rot[0][0]);
+		glDrawArrays(GL_LINES, model.getObjectStart(2), model.getObjectSize(2));
+
+		rot = glm::rotate(glm::mat4(1.0f), time * 12, glm::vec3(0, 0, 1));
+		glUniformMatrix4fv(transformSlot, 1, GL_FALSE, &rot[0][0]);
+		glDrawArrays(GL_LINES, model.getObjectStart(3), model.getObjectSize(3));
+
+		/*glm::mat4 ident = glm::mat4(1.0f);
 		glUniformMatrix4fv(transformSlot, 1, GL_FALSE, &ident[0][0]);
 		glDrawArrays(GL_LINES, model.getObjectStart(2), model.getObjectSize(2));
 
 		glm::mat4 funny = glm::translate(glm::mat4(1.0f), glm::vec3(time * 0.1, time * 0.1, 0));
 		funny = funny * rot;
 		glUniformMatrix4fv(transformSlot, 1, GL_FALSE, &funny[0][0]);
-		glDrawArrays(GL_LINES, model.getObjectStart(3), model.getObjectSize(3));
+		glDrawArrays(GL_LINES, model.getObjectStart(3), model.getObjectSize(3));*/
 		checkGLError("draw");
 		
 		//cleanup
